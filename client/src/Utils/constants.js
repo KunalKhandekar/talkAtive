678552a2aa7b6c0be422ed6ae027d3159145da8c
KunalKhandekar <kunalkhandekar010@gmail.com>
2024-08-19 18:23:00 +0530

    fallback

--- client/src/Utils/constants.js
+++ client/src/Utils/constants.js
@@ -1,4 +1,4 @@
 export const default_avatar = "https://i0.wp.com/sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png?ssl=1";
 
 
-export const backendUrl = "https://talk-ative.vercel.app";
+export const backendUrl = "https://talkative-2ld0.onrender.com";
